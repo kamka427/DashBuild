57c9177980e0a83dd841f25191fdd7ce066009e9
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-01-30 23:08:10 +0100

    Update Navigation component style with Tailwind

--- client/src/navigation.svelte
+++ client/src/navigation.svelte
@@ -1,55 +1,19 @@
-<nav>
-	<ul>
-		<li class="title"><a href="/">DashBuild</a></li>
-		<li class="navlink"><a href="/">Home</a></li>
-		<li class="navlink"><a href="/gallery">Gallery</a></li>
-		<li class="navlink"><a href="/dashboards">My Dashboards</a></li>
-	</ul>
-	<ul>
-		<li class="create"><a href="/create">Create</a></li>
-		<li class="profile"><a href="/profile">Profile</a></li>
-	</ul>
+<nav
+	class="flex shadow-lg justify-between items-center px-6 py-2 dark:bg-gray-800 dark:text-slate-100"
+>
+	<div class="flex gap-4 items-center">
+		<a class="text-3xl hover:underline" href="/">DashBuild</a>
+		<a class="text-xl hover:underline" href="/">Home</a>
+		<a class="text-xl hover:underline" href="/gallery">Gallery</a>
+		<a class="text-xl hover:underline" href="/dashboards">My Dashboards</a>
+	</div>
+	<div class="flex gap-4 items-center">
+		<a
+			class="text-slate-100 rounded-2xl shadow-lg bg-green-600 px-6 py-1 hover:bg-green-800"
+			href="/create">Create</a
+		>
+		<a
+			class="h-10 w-10 rounded-full shadow-lg bg-gray-800 hover:bg-gray-500 dark:bg-slate-400 dark:hover:bg-slate-700 text-center "
+			href="/profile"> </a>
+	</div>
 </nav>
-
-<style lang="postcss">
-	nav {
-		background: rgb(255, 255, 255);
-		box-shadow: 0 0 1em rgba(0, 0, 0, 0.5);
-		padding: 0.7em;
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.title {
-		font-size: 1.4em;
-		font-weight: semibold;
-	}
-
-	ul {
-		display: flex;
-		align-items: center;
-		list-style: none;
-		padding: 0;
-		margin: 0;
-	}
-
-	.navlink {
-		margin-left: 1em;
-	}
-
-	.create {
-		margin-left: 1em;
-		background: rgb(45, 142, 19);
-		color: rgb(255, 255, 255);
-		padding: 0.2em 1.4em;
-		border-radius: 1em;
-	}
-
-	.profile {
-		margin-left: 1em;
-		background: rgb(50, 50, 50);
-		color: rgb(255, 255, 255);
-		padding: 0.2em 1.4em;
-		border-radius: 1em;
-	}
-</style>
