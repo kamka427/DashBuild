f3d912d6092ca36a8da5cba5d52ca6dbc5ff9428
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-01-30 23:43:09 +0100

    Add placeholder text and buttons

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="m-12 space-y-6">
+	<div>
+		<h1 class="text-4xl">Welcome to DashBuild</h1>
+		<h2 class="text-2xl">Build. Dashboards. Easily.</h2>
+	</div>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, enim temporibus sequi illum
+		maxime asperiores iusto aperiam illo nesciunt, quidem perspiciatis! Odit, esse? Eum labore
+		aliquam amet possimus asperiores est. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+		Facilis explicabo minus accusantium eligendi, nostrum molestiae adipisci voluptatem minima rem
+		velit facere, nam, id cumque exercitationem autem quidem tenetur vel modi.
+	</p>
+	<div class="space-x-2">
+		<button class="text-slate-100 rounded-3xl shadow-lg bg-green-600 px-6 py-4 hover:bg-green-800">
+			Create a new Dashboard
+		</button>
+		<button class="text-slate-100 rounded-3xl shadow-lg bg-blue-600 px-6 py-4 hover:bg-blue-800">
+			Jump to my Dashboards
+		</button>
+	</div>
+</main>
