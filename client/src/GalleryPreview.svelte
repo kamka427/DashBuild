d6006db0b323362c7f013e29eff14251a00ee025
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-01-31 22:23:38 +0100

    Add GalleryPreview and Pagination components

---
+++ client/src/GalleryPreview.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Pagination from './Pagination.svelte';
+
+	// six random images from unsplash in a list
+	export let dashboards = [
+		'https://grafana.com/static/img/screenshots/Modal_dashboards.png',
+		'https://grafana.com/static/img/screenshots/Modal_dashboards.png',
+		'https://grafana.com/static/img/screenshots/Modal_dashboards.png',
+		'https://grafana.com/static/img/screenshots/Modal_dashboards.png',
+		'https://grafana.com/static/img/screenshots/Modal_dashboards.png',
+		'https://grafana.com/static/img/screenshots/Modal_dashboards.png'
+	];
+</script>
+
+<div class="rounded-xl bg-neutral-200 p-3 shadow-lg dark:bg-slate-500">
+	<h1 class="pb-3 text-center text-xl">Gallery of already built Dashboards</h1>
+	<div class="flex flex-wrap justify-center gap-8">
+		{#each dashboards as dash}
+			<img class="h-20 rounded-md shadow-lg" src={dash} alt="dashboard" />
+		{/each}
+	</div>
+	<Pagination />
+</div>
---
+++ client/src/Pagination.svelte
@@ -0,0 +1,10 @@
+<!-- three dot pagination -->
+<div>
+	<div class="flex justify-center p-3">
+		<div class="flex items-center gap-2">
+			<div class="h-4 w-4 rounded-full bg-gray-400" />
+			<div class="h-5 w-5 rounded-full bg-gray-400" />
+			<div class="h-4 w-4 rounded-full bg-gray-400" />
+		</div>
+	</div>
+</div>
