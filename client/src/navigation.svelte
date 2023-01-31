275760650a9500d350d4bdd2c8ee6dd854e29a3f
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-01-31 22:24:43 +0100

    Update styles for Landing page

--- client/src/navigation.svelte
+++ client/src/navigation.svelte
@@ -1,19 +1,20 @@
 <nav
-	class="flex shadow-lg justify-between items-center px-6 py-2 dark:bg-gray-800 dark:text-slate-100"
+	class="flex items-center justify-between px-6 py-2 shadow-lg dark:bg-gray-800 dark:text-slate-100"
 >
-	<div class="flex gap-4 items-center">
+	<div class="flex items-center gap-4">
 		<a class="text-3xl hover:underline" href="/">DashBuild</a>
 		<a class="text-xl hover:underline" href="/">Home</a>
 		<a class="text-xl hover:underline" href="/gallery">Gallery</a>
 		<a class="text-xl hover:underline" href="/dashboards">My Dashboards</a>
 	</div>
-	<div class="flex gap-4 items-center">
+	<div class="flex items-center gap-4">
 		<a
-			class="text-slate-100 rounded-2xl shadow-lg bg-green-600 px-6 py-1 hover:bg-green-800"
+			class="rounded-2xl bg-green-600 px-6 py-1 text-slate-100 shadow-lg hover:bg-green-800"
 			href="/create">Create</a
 		>
 		<a
-			class="h-10 w-10 rounded-full shadow-lg bg-gray-800 hover:bg-gray-500 dark:bg-slate-400 dark:hover:bg-slate-700 text-center "
-			href="/profile"> </a>
+			class="h-10 w-10 rounded-full bg-gray-800 text-center shadow-lg hover:bg-gray-500 dark:bg-slate-400 dark:hover:bg-slate-700 "
+			href="/profile"
+		/>
 	</div>
 </nav>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,21 +1,30 @@
-<main class="m-12 space-y-6">
-	<div>
-		<h1 class="text-4xl">Welcome to DashBuild</h1>
-		<h2 class="text-2xl">Build. Dashboards. Easily.</h2>
+<script>
+	import Minigallery from '../GalleryPreview.svelte';
+</script>
+
+<main class="m-12  flex dark:text-slate-100">
+	<div class="space-y-6">
+		<div>
+			<h1 class="text-4xl">Welcome to DashBuild</h1>
+			<h2 class="text-2xl">Build. Dashboards. Easily.</h2>
+		</div>
+		<p class="w-[80%] text-lg">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, enim temporibus sequi illum
+			maxime asperiores iusto aperiam illo nesciunt, quidem perspiciatis! Odit, esse? Eum labore
+			aliquam amet possimus asperiores est. Lorem, ipsum dolor sit amet consectetur adipisicing
+			elit. Facilis explicabo minus accusantium eligendi, nostrum molestiae adipisci voluptatem
+			minima rem velit facere, nam, id cumque exercitationem autem quidem tenetur vel modi.
+		</p>
+		<div class="space-x-2">
+			<button
+				class="rounded-3xl bg-green-600 px-6 py-4 text-slate-100 shadow-lg hover:bg-green-800"
+			>
+				Create a new Dashboard
+			</button>
+			<button class="rounded-3xl bg-blue-600 px-6 py-4 text-slate-100 shadow-lg hover:bg-blue-800">
+				Jump to my Dashboards
+			</button>
+		</div>
 	</div>
-	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, enim temporibus sequi illum
-		maxime asperiores iusto aperiam illo nesciunt, quidem perspiciatis! Odit, esse? Eum labore
-		aliquam amet possimus asperiores est. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-		Facilis explicabo minus accusantium eligendi, nostrum molestiae adipisci voluptatem minima rem
-		velit facere, nam, id cumque exercitationem autem quidem tenetur vel modi.
-	</p>
-	<div class="space-x-2">
-		<button class="text-slate-100 rounded-3xl shadow-lg bg-green-600 px-6 py-4 hover:bg-green-800">
-			Create a new Dashboard
-		</button>
-		<button class="text-slate-100 rounded-3xl shadow-lg bg-blue-600 px-6 py-4 hover:bg-blue-800">
-			Jump to my Dashboards
-		</button>
-	</div>
+	<Minigallery />
 </main>
