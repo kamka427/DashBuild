9f6f90bbee1af628f421dfe37c4b20d11dc4f425
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-02-04 22:40:52 +0100

    Add labels for grid inputs

--- client/src/routes/create/+page.svelte
+++ client/src/routes/create/+page.svelte
@@ -1,22 +1,30 @@
 <script>
+	import PanelDetails from '../../components/PanelEdit.svelte';
 	import PanelView from '../../components/PanelView.svelte';
 </script>
 
 <main>
-	<div class="container m-12 dark:text-white">
+	<div class="container mx-auto my-12 dark:text-white">
 		<div class="flex justify-between">
 			<h1 class="text-4xl">Create Dashboard</h1>
 			<div class="flex gap-2">
-				<input
-					type="number"
-					placeholder="Columns"
-					class="rounded-md bg-gray-200 px-6 py-4 text-black shadow-lg hover:bg-gray-300"
-				/>
-				<input
-					type="number"
-					placeholder="Rows"
-					class="rounded-md bg-gray-200 px-6 py-4 text-black shadow-lg hover:bg-gray-300"
-				/>
+				<div class="flex items-center gap-2">
+					<label for="columns">Columns:</label>
+					<input
+						id="columns"
+						type="number"
+						placeholder="2"
+						class="h-8 w-12 rounded-md bg-gray-200 text-center text-black shadow-lg hover:bg-gray-300"
+					/>
+				</div>
+				<div class="flex items-center gap-2">
+					<label for="Rows">Rows:</label>
+					<input
+						type="number"
+						placeholder="2"
+						class="h-8 w-12 rounded-md bg-gray-200 text-center text-black shadow-lg hover:bg-gray-300"
+					/>
+				</div>
 			</div>
 		</div>
 		<div>
