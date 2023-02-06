43eaf62616b8b674bc0eb449bb90f576bf7743f0
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-02-06 22:48:14 +0100

    Wrap SVGs in button elements

--- client/src/components/PanelView.svelte
+++ client/src/components/PanelView.svelte
@@ -1,20 +1,22 @@
 <div class="flex w-1/3 flex-col gap-3 rounded-md bg-gray-200 p-3 text-black shadow-xl">
 	<div class="flex items-center justify-between">
 		<h1 class="text-xl">Panel Title</h1>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="h-8 w-8 cursor-pointer text-red-800"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="4"
-				d="M6 18L18 6M6 6l12 12"
-			/>
-		</svg>
+		<button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-8 w-8 cursor-pointer text-red-800"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="4"
+					d="M6 18L18 6M6 6l12 12"
+				/>
+			</svg>
+		</button>
 	</div>
 	<img
 		src="https://grafana.com/static/img/screenshots/Modal_dashboards.png"
