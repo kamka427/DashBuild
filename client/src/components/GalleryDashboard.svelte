ee12b9c1538092ff88faafcf76102b6ff42a68ff
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-02-09 22:47:42 +0100

    Add GalleryDashboard component

---
+++ client/src/components/GalleryDashboard.svelte
@@ -0,0 +1,22 @@
+<div class="flex flex-col gap-3 rounded-md bg-gray-200 p-3 text-black shadow-xl">
+	<div class="flex items-center justify-between">
+		<h1 class="text-xl">Dashboard Title</h1>
+		<div class="flex items-center gap-6">
+			<p>Team</p>
+			<p class="rounded-md bg-gray-400 p-2">Tags...</p>
+		</div>
+	</div>
+	<img
+		src="https://grafana.com/static/img/screenshots/Modal_dashboards.png"
+		class="w-max rounded-md bg-gray-300 shadow-xl"
+		alt=""
+	/>
+	<div class="flex flex-col justify-end gap-3 lg:flex-row">
+		<button class="rounded-md bg-cyan-600 px-8 py-2 text-white shadow-lg hover:bg-green-800 "
+			>View</button
+		>
+		<button class="rounded-md bg-green-600 px-8 py-2 text-white shadow-lg hover:bg-green-800"
+			>Copy</button
+		>
+	</div>
+</div>
--- client/src/routes/gallery/+page.svelte
+++ client/src/routes/gallery/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Pagination from '../../components/Pagination.svelte';
+	import GalleryDashboard from '../../components/GalleryDashboard.svelte';
 
 	let tags = ['val1', 'val2', 'val3', 'val4'];
 </script>
@@ -7,29 +8,32 @@
 <main>
 	<div class="container mx-auto my-12 space-y-6 dark:text-white">
 		<div class="flex-col space-y-6">
-			<div class="flex flex-col justify-between gap-2 lg:flex-row items-center">
+			<div class="flex flex-col items-center justify-between gap-2 lg:flex-row">
 				<h1 class="text-4xl">Gallery</h1>
 				<input
 					type="search"
 					placeholder="Search for a dashboard"
-					class="h-8 rounded-md w-1/2 bg-gray-200 px-2 text-black shadow-lg hover:bg-gray-300"
+					class="h-8 w-1/2 rounded-md bg-gray-200 px-2 text-black shadow-lg hover:bg-gray-300"
 				/>
-                <div class="flex gap-4">
-				<select class="text-black h-8 px-2">
-					<option value="none">Tags</option>
-					{#each tags as tag}
-						<option value={tag}>{tag}</option>
-					{/each}
-				</select>
-				<select class="text-black h-8 px-2">
-					<option value="none">Teams</option>
-					{#each tags as tag}
-                        <option value={tag}>{tag}</option>
-                    {/each}
-				</select>
-                </div>
+				<div class="flex gap-4">
+					<select class="h-8 px-2 text-black">
+						<option value="none">Tags</option>
+						{#each tags as tag}
+							<option value={tag}>{tag}</option>
+						{/each}
+					</select>
+					<select class="h-8 px-2 text-black">
+						<option value="none">Teams</option>
+						{#each tags as tag}
+							<option value={tag}>{tag}</option>
+						{/each}
+					</select>
+				</div>
 			</div>
-			<div class="grid grid-cols-2 grid-rows-1 place-items-center gap-3" />
+			<div class="grid grid-cols-2 grid-rows-1 place-items-center gap-3">
+				<GalleryDashboard />
+				<GalleryDashboard />
+			</div>
 			<Pagination />
 		</div>
 	</div>
