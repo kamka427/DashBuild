702fe928ef8716ead5d4f3d70dfa4c227a175bff
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-02-09 22:15:49 +0100

    Fix 404 error centering

--- client/src/routes/+error.svelte
+++ client/src/routes/+error.svelte
@@ -1,4 +1,4 @@
 <!-- 404 error page -->
-<main class="container m-12 text-center dark:text-slate-100">
+<main class="container mx-auto my-24 text-center dark:text-slate-100">
 	<h1 class="bold rounded-md text-4xl ">404 - Page Not Found</h1>
 </main>
