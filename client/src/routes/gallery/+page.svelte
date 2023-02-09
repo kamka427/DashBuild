71af54fa25b34482d01df51458a0072d9d7995a5
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-02-09 22:36:29 +0100

    Add filters layout to Gallery

---
+++ client/src/routes/gallery/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Pagination from '../../components/Pagination.svelte';
+
+	let tags = ['val1', 'val2', 'val3', 'val4'];
+</script>
+
+<main>
+	<div class="container mx-auto my-12 space-y-6 dark:text-white">
+		<div class="flex-col space-y-6">
+			<div class="flex flex-col justify-between gap-2 lg:flex-row items-center">
+				<h1 class="text-4xl">Gallery</h1>
+				<input
+					type="search"
+					placeholder="Search for a dashboard"
+					class="h-8 rounded-md w-1/2 bg-gray-200 px-2 text-black shadow-lg hover:bg-gray-300"
+				/>
+                <div class="flex gap-4">
+				<select class="text-black h-8 px-2">
+					<option value="none">Tags</option>
+					{#each tags as tag}
+						<option value={tag}>{tag}</option>
+					{/each}
+				</select>
+				<select class="text-black h-8 px-2">
+					<option value="none">Teams</option>
+					{#each tags as tag}
+                        <option value={tag}>{tag}</option>
+                    {/each}
+				</select>
+                </div>
+			</div>
+			<div class="grid grid-cols-2 grid-rows-1 place-items-center gap-3" />
+			<Pagination />
+		</div>
+	</div>
+</main>
