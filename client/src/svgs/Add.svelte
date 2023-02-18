bca93367b007da24e339f888a342676ae3b89863
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-02-18 19:06:29 +0100

    Add svg components for reusability

---
+++ client/src/svgs/Add.svelte
@@ -0,0 +1,15 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="h-64 w-64 text-gray-400"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke="currentColor"
+	filter="drop-shadow(4px 4px 2px rgb(0 0 0 / 0.2))"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="1"
+		d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+	/>
+</svg>
