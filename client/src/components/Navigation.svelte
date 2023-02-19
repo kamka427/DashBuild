1f7077d8174d7858386bb56440140fe21e8510bd
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-02-19 16:40:46 +0100

    Update Navigation with session handling

--- client/src/components/Navigation.svelte
+++ client/src/components/Navigation.svelte
@@ -1,20 +1,49 @@
+<script>
+	import { page } from '$app/stores';
+	import Close from '../svgs/Close.svelte';
+</script>
+
 <nav class="py-2 shadow-lg dark:bg-gray-800 dark:text-slate-100">
 	<div class="container mx-auto flex items-center justify-between">
 		<div class="flex items-center gap-4">
 			<a class="text-3xl hover:underline" href="/">DashBuild</a>
-			<a class="text-xl hover:underline" href="/">Home</a>
-			<a class="text-xl hover:underline" href="/gallery">Gallery</a>
-			<a class="text-xl hover:underline" href="/dashboards">My Dashboards</a>
+			{#if $page.data.session}
+				<a class="text-xl hover:underline" href="/">Home</a>
+				<a class="text-xl hover:underline" href="/protected/gallery">Gallery</a>
+				<a class="text-xl hover:underline" href="/protected/dashboards">My Dashboards</a>
+			{/if}
 		</div>
-		<div class="flex items-center gap-4">
-			<a
-				class="rounded-2xl bg-green-600 px-6 py-1 text-slate-100 shadow-lg hover:bg-green-800"
-				href="/create">Create</a
-			>
-			<a
-				class="h-10 w-10 rounded-full bg-gray-800 text-center shadow-lg hover:bg-gray-500 dark:bg-slate-400 dark:hover:bg-slate-700"
-				href="/profile"
-			/>
-		</div>
+		{#if $page.data.session}
+			<div class="flex items-center gap-4">
+				<a
+					class="rounded-2xl bg-green-600 px-6 py-1 text-slate-100 shadow-lg hover:bg-green-800"
+					href="/protected/create">Create</a
+				>
+				<span>
+					<p class="text-sm">Signed in as</p>
+					<a href="/protected/profile" class="text-md hover:underline">{$page.data.session.user?.name}</a>
+				</span>
+				{#if $page.data.session.user?.image}
+					<img src={$page.data.session.user.image} alt="" class="h-8 w-8 rounded-full" />
+				{/if}
+
+				<a href="/auth/signout"><Close /></a>
+			</div>
+		{:else}
+			<div class="flex items-center gap-4">
+				<a
+					class="flex gap-2 rounded-2xl bg-green-600 px-6 py-1 text-slate-100 shadow-lg hover:bg-green-800"
+					href="/auth/signin"
+				>
+					Log in
+				</a>
+				<a
+					class="flex gap-2 rounded-2xl bg-blue-600 px-6 py-1 text-slate-100 shadow-lg hover:bg-blue-800"
+					href="/auth/register"
+				>
+					Register
+				</a>
+			</div>
+		{/if}
 	</div>
 </nav>
