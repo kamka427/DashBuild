7114a3884d960e3ddf2900ab8087029756b609f6
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-06 22:15:17 +0100

    Add fake test data generation to seeder

--- client/prisma/seed.ts
+++ client/prisma/seed.ts
@@ -1,17 +1,57 @@
 /// <reference types="node" />
 import { PrismaClient } from '@prisma/client';
-import {faker} from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 async function main() {
-  const fakeUsers = Array.from({ length: 10 }).map(() => ({
-    name: faker.name.fullName(),
-    email: faker.internet.email(),
-    team: "team" + faker.helpers.arrayElement(['A', 'B', 'C', 'D', 'E']),
-  }));
-  await prisma.user.createMany({
-    data: fakeUsers,
-  });
+	await prisma.panelsOnDashboards.deleteMany({});
+	await prisma.dashboard.deleteMany({});
+	await prisma.panel.deleteMany({});
+	await prisma.user.deleteMany({});
+
+	const fakeUsers = Array.from({ length: 10 }).map(() => ({
+		id: faker.datatype.uuid(),
+		name: faker.name.fullName(),
+		email: faker.internet.email(),
+		team: 'team' + faker.helpers.arrayElement(['A', 'B', 'C', 'D', 'E'])
+	}));
+	await prisma.user.createMany({
+		data: fakeUsers
+	});
+
+	const fakeDashboards = Array.from({ length: 10 }).map(() => ({
+		id: faker.datatype.uuid(),
+		name: faker.commerce.productName(),
+		description: faker.commerce.productDescription(),
+		published: faker.datatype.boolean(),
+		thumbnail: Buffer.from(faker.image.imageUrl()),
+		userId: faker.helpers.arrayElement(fakeUsers).id,
+		representation: {}
+	}));
+	await prisma.dashboard.createMany({
+		data: fakeDashboards
+	});
+
+	const fakePanels = Array.from({ length: 10 }).map(() => ({
+		id: faker.datatype.uuid(),
+		name: faker.commerce.productName(),
+		description: faker.commerce.productDescription(),
+		thumbnail: Buffer.from(faker.image.imageUrl()),
+		representation: {}
+	}));
+
+	await prisma.panel.createMany({
+		data: fakePanels
+	});
+
+	const fakeDashboardPanels = Array.from({ length: 10 }).map(() => ({
+		dashboardId: faker.helpers.arrayElement(fakeDashboards).id,
+		panelId: faker.helpers.arrayElement(fakePanels).id
+	}));
+
+	await prisma.panelsOnDashboards.createMany({
+		data: fakeDashboardPanels
+	});
 }
 main()
 	.then(async () => {
