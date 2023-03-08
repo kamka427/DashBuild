a4cc97c8c73a891694a49a8c00729c49faa97dd3
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-08 22:57:55 +0100

    Add dynamic props to DashboardView

--- client/src/components/DashboardView.svelte
+++ client/src/components/DashboardView.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-	import Circle from '../svgs/Circle.svelte';
+	import type { Dashboard } from '@prisma/client';
+	import Published from '../components/Published.svelte';
 	import Close from '../svgs/Close.svelte';
 
-	export let source: string;
+	export let dashboard: Dashboard;
 </script>
 
 <div class="flex flex-col gap-3 rounded-md bg-gray-200 p-3 text-black shadow-xl">
 	<div class="flex items-center justify-between">
-		<h1 class="text-xl">Dashboard Title</h1>
+		<h1 class="text-xl">{dashboard.name}</h1>
 		<div class="flex items-center">
-			<p class="text-">Published</p>
 			<button>
-				<Circle />
+				<Published published={dashboard.published} />
 			</button>
 			<button>
 				<Close />
 			</button>
 		</div>
 	</div>
-	<img src={source} class="w-max rounded-md bg-gray-300 shadow-xl" alt="" />
+	<img src={dashboard.preview} class="w-max rounded-md bg-gray-300 shadow-xl" alt="" />
 	<div class="flex flex-col justify-end gap-3 lg:flex-row">
 		<button class="rounded-md bg-cyan-600 px-8 py-2 text-white shadow-lg hover:bg-cyan-800 "
 			>View</button
---
+++ client/src/components/Published.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let published: boolean;
+
+	const togglePublished = () => {
+		published = !published;
+	};
+</script>
+
+<div class="flex items-center justify-center">
+	<button on:click={togglePublished}>
+		{#if published}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="green"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+			</svg>
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="red"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M6 18L18 6M6 6l12 12"
+				/>
+			</svg>
+		{/if}
+	</button>
+</div>
