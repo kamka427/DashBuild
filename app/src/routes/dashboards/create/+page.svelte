5e8e663cb29fa38fd6e9f1ae7c94a8c2a12a297b
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-19 20:16:45 +0100

    Update create page with dynamic data

--- app/src/routes/dashboards/create/+page.svelte
+++ app/src/routes/dashboards/create/+page.svelte
@@ -1,68 +1,43 @@
-<script>
-	import NewPanelCard from '$lib/components/NewPanelCard.svelte';
-	import PanelEditorCard from '$lib/components/PanelEditorCard.svelte';
+<script lang="ts">
+	import DashboardProperties from '$lib/components/DashboardProperties.svelte';
 	import PanelCard from '$lib/components/PanelCard.svelte';
+	import BreadCrumbs from '$lib/components/BreadCrumbs.svelte';
 
-	export let panels = [
-		'../src/lib/placeholders/panel1.png',
-		'../src/lib/placeholders/panel2.png',
-		'../src/lib/placeholders/panel3.png',
-		'../src/lib/placeholders/panel4.png'
-	];
+	interface Data {
+		panels: {
+			id: string;
+			name: string;
+			description: string;
+			preview: string;
+			representation: string;
+		}[];
+	}
+
+	let dashboardName = '';
+	let rowCount = 2;
+	let colCount = 2;
+
+	export let data: Data;
+
+	export let panelForm = data.panels
+
 </script>
 
 <svelte:head>
 	<title>Create Dashboard</title>
 </svelte:head>
-<main>
-	<div class="container mx-auto my-12 space-y-6 dark:text-white">
-		<div class="flex-col space-y-6">
-			<div class="flex flex-col justify-between gap-2 lg:flex-row">
-				<h1 class="text-4xl">Create Dashboard</h1>
-				<div class="flex flex-col gap-2 sm:flex-row">
-					<div class="flex items-center gap-2">
-						<label class="font-semibold" for="dashboardtitle">Dashboard Title:</label>
-						<input
-							id="dashboardtitle"
-							type="text"
-							placeholder="Title of the dashboard"
-							class="h-8 rounded-md bg-gray-200 px-2 text-black shadow-lg hover:bg-gray-300"
-						/>
-					</div>
-					<div class="flex items-center gap-2">
-						<label class="font-semibold" for="columns">Columns:</label>
-						<input
-							id="columns"
-							type="number"
-							placeholder="2"
-							class="h-8 w-12 rounded-md bg-gray-200 text-center text-black shadow-lg hover:bg-gray-300"
-						/>
-					</div>
-					<div class="flex items-center gap-2">
-						<label class="font-semibold" for="Rows">Rows:</label>
-						<input
-							type="number"
-							placeholder="2"
-							class="h-8 w-12 rounded-md bg-gray-200 text-center text-black shadow-lg hover:bg-gray-300"
-						/>
-					</div>
-				</div>
-			</div>
-			<div class="grid grid-cols-2 grid-rows-2 place-items-center gap-3">
-				{#each panels as panel}
-					<PanelCard source={panel} />
-				{/each}
-				<PanelEditorCard source="../src/lib/placeholders/panel3.png" />
-				<NewPanelCard />
-			</div>
-			<div class="flex flex-col justify-end gap-3 sm:flex-row">
-				<button class="rounded-md bg-red-600 px-8 py-2 text-white shadow-lg hover:bg-red-800">
-					Discard
-				</button>
-				<button class="rounded-md bg-green-600 px-8 py-2 text-white shadow-lg hover:bg-green-800">
-					Create
-				</button>
-			</div>
+<main class="container mx-auto space-y-6">
+	<div class="flex flex-col items-center justify-between gap-2 lg:flex-row">
+		<BreadCrumbs location="Create Dashboard" />
+		<DashboardProperties bind:dashboardName bind:rowCount bind:colCount />
+		<div class="btn-group">
+			<button class="btn btn-secondary">Discard</button>
+			<button class="btn btn-primary">Save Dashboard</button>
 		</div>
+	</div>
+	<div class="grid grid-cols-{colCount} grid-rows-{rowCount} place-items-start gap-2">
+		{#each panelForm as panel}
+			<PanelCard {panel} />
+		{/each}	
 	</div>
 </main>
