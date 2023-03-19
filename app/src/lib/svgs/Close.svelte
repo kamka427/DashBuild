005baf62271d45bd4458b3a58303f08c80c78c68
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-19 20:08:17 +0100

    Remove unused svgs

--- app/src/lib/svgs/Close.svelte
+++
@@ -1,9 +0,0 @@
-<svg
-	xmlns="http://www.w3.org/2000/svg"
-	class="h-8 w-8 cursor-pointer text-red-800"
-	fill="none"
-	viewBox="0 0 24 24"
-	stroke="currentColor"
->
-	<path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M6 18L18 6M6 6l12 12" />
-</svg>
