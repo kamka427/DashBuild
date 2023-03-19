cc508a595349b778c82330332b2a7a3a884042c8
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-19 20:07:34 +0100

    Add safelist to Tailwind config

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -4,5 +4,11 @@ module.exports = {
 	theme: {
 		extend: {}
 	},
-	plugins: [require("daisyui")]
+	plugins: [require('daisyui')],
+	safelist: [
+		'grid-cols-1',
+		'grid-cols-2',
+		'grid-cols-3',
+		'grid-cols-4',
+	]
 };
