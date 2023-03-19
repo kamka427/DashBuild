fef3b81a7165e4bf37260a8da638bb59e863c079
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-19 20:14:15 +0100

    Update Landing page layout

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -8,6 +8,7 @@ export const load: PageServerLoad = async () => {
 		dashboards: prisma.dashboard.findMany({
 			select: {
 				id: true,
+				name: true,
 				preview: true
 			},
 			where: {
