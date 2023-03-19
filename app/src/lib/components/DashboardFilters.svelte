b8f736040ae0f8e7cddd63a345f898e8dc9f9a3f
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-19 20:13:06 +0100

    Update DashboardFilters design and functionality

--- app/src/lib/components/DashboardFilters.svelte
+++ app/src/lib/components/DashboardFilters.svelte
@@ -12,42 +12,50 @@
 	export let search: string;
 	export let tag: string;
 	export let team: string;
+	export let columns: number;
 
 	export const teams = Array.from(new Set(data.teams.map((team) => team.team)));
 	export const tags = Array.from(new Set(data.tags.map((tag) => tag.tags).flat()));
 </script>
 
-<input
-	type="search"
-	placeholder="Search for a dashboard"
-	class="h-12 w-1/2 rounded-md bg-gray-200 px-2 text-black shadow-lg hover:bg-gray-300 dark:bg-zinc-700 dark:text-white dark:hover:bg-zinc-600"
-	bind:value={search}
-/>
-<div class="flex gap-4">
-	<div>
-		<label for="tag" class="text-xl">Tags:</label>
-		<select
-			class="h-12 rounded-md bg-gray-200 p-2 text-black shadow-lg hover:bg-gray-300 dark:bg-zinc-700 dark:text-white dark:hover:bg-zinc-600"
-			id="tag"
-			bind:value={tag}
-		>
-			<option value="none">None</option>
+<div class="flex flex-row gap-4">
+	<label class="input-group">
+		<span>Search</span>
+		<input
+			type="search"
+			placeholder="Search for a dashboard"
+			class="input-bordered input"
+			bind:value={search}
+		/>
+	</label>
+	<label class="input-group">
+		<span>Tag</span>
+		<select class="select-bordered select" id="tag" bind:value={tag}>
+			<option selected value="none">Filter by Tag</option>
 			{#each tags as tag}
 				<option value={tag}>{tag}</option>
 			{/each}
 		</select>
-	</div>
-	<div>
-		<label for="team" class="text-xl">Teams:</label>
-		<select
-			class="h-12 rounded-md bg-gray-200 p-2 text-black shadow-lg hover:bg-gray-300 dark:bg-zinc-700 dark:text-white dark:hover:bg-zinc-600"
-			id="team"
-			bind:value={team}
-		>
-			<option value="none">None</option>
+	</label>
+	<label class="input-group">
+		<span>Team</span>
+		<select class="select-bordered select" id="team" bind:value={team}>
+			<option selected value="none">Filter by Team</option>
 			{#each teams as team}
 				<option value={team}>{team}</option>
 			{/each}
 		</select>
-	</div>
+	</label>
+	<label class="input-group">
+		<span>Columns</span>
+		<input
+			class="input input-bordered"
+			id="team"
+			bind:value={columns}
+			min="1"
+			max="4"
+			type="number"
+			placeholder="2"
+		/>
+	</label>
 </div>
