8a43940e8928b7e4adb981f1e6cbb174f0079425
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-19 20:17:12 +0100

    Extend DashboardCard component

--- app/src/lib/components/DashboardCard.svelte
+++ app/src/lib/components/DashboardCard.svelte
@@ -4,45 +4,55 @@
 	import { page } from '$app/stores';
 	const currentPage = $page.url.pathname.split('/')[2];
 	export let dashboard: Dashboard & { user: { team: string } };
+
+	const shortDescription = dashboard.description
+		? dashboard.description.length > 120
+			? dashboard.description.substring(0, 120) + '...'
+			: dashboard.description
+		: 'No description provided';
 </script>
 
-<main
-	class="flex flex-col gap-3 rounded-md bg-gray-200 p-3 text-black shadow-xl dark:bg-zinc-700 dark:text-white"
->
-	<section class="flex items-center justify-between">
-		<h1 class="text-xl">{dashboard.name} [ver. {dashboard.version}]</h1>
-		<section class="flex items-center gap-6">
+<main class="card card-compact bg-base-300 text-base-content h-full w-full">
+	<figure>
+		<img src={dashboard.preview} class="w-max shadow-xl" alt="" />
+	</figure>
+	<div class="card-body gap-4">
+		<h1 class="card-title">{dashboard.name}</h1>
+		<p>{shortDescription}</p>
+		<div class="stats shadow">
+			<div class="stat flex flex-row">
+				<div class="stat-title text-sm">Version</div>
+				<div class="stat-value text-sm">{dashboard.version}</div>
+			</div>
+
 			{#if currentPage === 'gallery'}
-				<p class="rounded-md bg-gray-300 p-2 shadow-md dark:bg-zinc-500">
-					Team: {dashboard.user.team}
-				</p>
+				<div class="stat flex flex-row">
+					<div class="stat-title text-sm">Team</div>
+					<div class="stat-value text-sm">{dashboard.user.team}</div>
+				</div>
 			{/if}
-			<p class="rounded-md bg-gray-300 p-2 shadow-md dark:bg-zinc-500">
-				Tags: {dashboard.tags.join(' | ')}
-			</p>
+			<div class="stat flex flex-row">
+				<div class="stat-title text-sm">Tags</div>
+				<div class="stat-value text-sm">{dashboard.tags.join(', ')}</div>
+			</div>
 			{#if currentPage === 'my-dashboards'}
 				<Published published={dashboard.published} dashboardId={dashboard.id} />
 			{/if}
-		</section>
-	</section>
-	<img src={dashboard.preview} class="w-max rounded-md shadow-xl" alt="" />
-	<p>{dashboard.description}</p>
-	<section class="flex flex-col justify-end gap-3 lg:flex-row">
-		{#if currentPage === 'gallery'}
-			<button class="rounded-md bg-cyan-600 px-8 py-2 text-white shadow-lg hover:bg-cyan-800"
-				>Copy</button
-			>
-		{/if}
-		{#if currentPage === 'my-dashboards'}
-			<button class="rounded-md bg-red-600 px-8 py-2 text-white shadow-lg hover:bg-red-800"
-				>Delete</button
-			>
-			<button class="rounded-md bg-cyan-600 px-8 py-2 text-white shadow-lg hover:bg-cyan-800"
-				>Modify</button
-			>
-		{/if}
-		<button class="rounded-md bg-green-600 px-8 py-2 text-white shadow-lg hover:bg-green-800 "
-			>View</button
-		>
-	</section>
+		</div>
+		<div class="card-actions justify-end">
+			<form action="?/deleteDashboard" method="POST">
+				<input type="hidden" name="dashboardId" value={dashboard.id} />
+				<div class="btn-group">
+					{#if currentPage === 'gallery'}
+						<button class="btn-secondary btn">Copy</button>
+					{/if}
+					{#if currentPage === 'my-dashboards'}
+						<button class="btn-error btn" type="submit">Delete</button>
+						<button class="btn-secondary btn">Modify</button>
+					{/if}
+					<button class="btn-primary btn">View</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </main>
