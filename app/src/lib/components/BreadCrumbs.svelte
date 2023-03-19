ea705044f540fa9b0c748bbb951ff63949628404
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-03-19 20:08:56 +0100

    Add BreadCrumbs component

---
+++ app/src/lib/components/BreadCrumbs.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let location: string;
+</script>
+
+<div class="breadcrumbs text-base-content">
+	<ul>
+		<li class="text-3xl"><a href="/">Home</a></li>
+		<li class="text-4xl">{location}</li>
+	</ul>
+</div>
