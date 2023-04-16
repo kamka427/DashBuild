2075e7830ccce68c0c0ed88176cbb508510069b7
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-04-16 16:53:19 +0200

    Extend NewPanelCard with type selection

--- app/src/lib/components/NewPanelCard.svelte
+++ app/src/lib/components/NewPanelCard.svelte
@@ -1,23 +1,74 @@
-<div class="card-compact card w-full bg-base-300 text-base-content">
-	<figure class="flex h-full w-full items-center justify-center bg-base-200">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="h-64 w-64 text-base-content"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-			filter="drop-shadow(4px 4px 2px rgb(0 0 0 / 0.2))"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1"
-				d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-			/>
-		</svg>
-	</figure>
-	<div class="card-body">
-		<h2 class="card-title">Add a new panel</h2>
-		<p>Click here to add a new panel to your dashboard</p>
-	</div>
+<script lang="ts">
+	export let panels: string[];
+
+	type state = 'selected' | 'unselected';
+	export let state = 'unselected';
+
+	function toggleState() {
+		if (state === 'unselected') {
+			state = 'selected';
+		} else {
+			state = 'unselected';
+		}
+	}
+
+	export let addAction: any;
+
+	export let selectedType: string;
+
+	function setSelection(type: string) {
+		selectedType = type;
+	}
+</script>
+
+<div class="card-compact card bg-base-300 text-base-content w-full">
+	{#if state === 'unselected'}
+		<figure class="bg-base-200 flex h-full w-full items-center justify-center">
+			<button on:click={toggleState}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="text-base-content h-64 w-64"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					filter="drop-shadow(4px 4px 2px rgb(0 0 0 / 0.2))"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="1"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+					/>
+				</svg>
+			</button>
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Add a new panel</h2>
+			<p>Click here to add a new panel to your dashboard</p>
+		</div>
+	{/if}
+	{#if state === 'selected'}
+		<div class="card-body">
+			<h2 class="card-title">Select the new panel</h2>
+			<ul class="menu bg-base-100 rounded-md">
+				{#each panels as panel}
+					<li>
+						<button
+							on:click={() => {
+								setSelection(panel);
+								addAction(panel);
+								toggleState();
+							}}
+							class="btn btn-ghost rounded-md"
+						>
+							{panel}
+						</button>
+					</li>
+				{/each}
+				<li>
+					<button class="btn btn-secondary rounded-md" on:click={toggleState}>Discard</button>
+				</li>
+			</ul>
+		</div>
+	{/if}
 </div>
