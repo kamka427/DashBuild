15853b1253f03e9a56dee00e146f13e42ceb7db4
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-05-09 21:41:19 +0200

    Add dynamic provider loading

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -1,11 +1,58 @@
+// @ts-nocheck
 import { SvelteKitAuth } from '@auth/sveltekit';
-import Email, { type SendVerificationRequestParams } from '@auth/core/providers/email';
+import Email from '@auth/core/providers/email';
 import Azure from '@auth/core/providers/azure-ad';
-import { AZURE_ID, AZURE_SECRET, AZURE_TENANT_ID, AUTH_SECRET } from '$env/static/private';
+import GitHub from '@auth/core/providers/github';
+import CredentialsProvider from '@auth/core/providers/credentials';
+import {
+	AZURE_ID,
+	AZURE_SECRET,
+	AZURE_TENANT_ID,
+	GITHUB_ID,
+	GITHUB_SECRET,
+	SMTP_HOST,
+	SMTP_PORT,
+	SMTP_USER,
+	SMTP_PASSWORD,
+	EMAIL_FROM,
+	AUTH_SECRET
+} from '$env/static/private';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { prisma } from '$lib/utils/prisma';
+
+const availableProviders = [];
+
+if (SMTP_HOST && SMTP_PORT && SMTP_USER && SMTP_PASSWORD && EMAIL_FROM) {
+	availableProviders.push(
+		Email({
+			server: {
+				host: SMTP_HOST,
+				port: Number(SMTP_PORT),
+				auth: {
+					user: SMTP_USER,
+					pass: SMTP_PASSWORD
+				}
+			},
+			from: EMAIL_FROM,
+			name: 'Sign in with Magic Link'
+		}) as any
+	);
+}
+if (AZURE_ID && AZURE_SECRET && AZURE_TENANT_ID) {
+	availableProviders.push(
+		Azure({
+			clientId: AZURE_ID,
+			clientSecret: AZURE_SECRET,
+			tenantId: AZURE_TENANT_ID,
+			authorization: { params: { scope: 'openid profile user.Read email' } }
+		}) as any
+	);
+}
+if (GITHUB_ID && GITHUB_SECRET) {
+	availableProviders.push(GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }) as any);
+}
 
 async function authorization({ event, resolve }: { event: any; resolve: any }): Promise<Response> {
 	if (event.url.pathname.startsWith('/p')) {
@@ -25,24 +72,25 @@ export const handle = sequence(
 	SvelteKitAuth({
 		adapter: PrismaAdapter(prisma) as any,
 		providers: [
-			Email({
-				server: {
-					host: process.env.SMTP_HOST,
-					port: Number(process.env.SMTP_PORT),
-					auth: {
-						user: process.env.SMTP_USER,
-						pass: process.env.SMTP_PASSWORD
-					}
+			CredentialsProvider({
+				name: 'Email for testing',
+				credentials: {
+					email: { label: 'Email', type: 'email' }
 				},
-				from: process.env.EMAIL_FROM,
-				name: 'Sign in with Email'
+				async authorize(credentials) {
+					const user = await prisma.user.findUnique({
+						where: {
+							email: credentials.email as string
+						}
+					});
+					if (user) {
+						return user;
+					} else {
+						return null;
+					}
+				}
 			}) as any,
-			Azure({
-				clientId: AZURE_ID,
-				clientSecret: AZURE_SECRET,
-				tenantId: AZURE_TENANT_ID,
-				authorization: { params: { scope: 'openid profile user.Read email' } }
-			}) as any
+			...availableProviders
 		],
 		secret: AUTH_SECRET,
 		trustHost: true,
