8a8d9a4a3e8acb9aacf89b7490c7111449c378b2
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-05-10 23:01:11 +0200

    Fix not working redirect

--- app/src/routes/p/(editor)/(update)/copy/[...dashboardId]/+page.svelte
+++ app/src/routes/p/(editor)/(update)/copy/[...dashboardId]/+page.svelte
@@ -1,3 +1,40 @@
+<script lang="ts">
+	import DashboardEditor from '$lib/components/DashboardEditor.svelte';
+	import BreadCrumbs from '$lib/components/BreadCrumbs.svelte';
+	import type { Panel } from '@prisma/client';
+	import type { ActionData, PageData } from './copy/[...dashboardId]/$types';
+	import Helper from '$lib/components/Helper.svelte';
+
+	export let data: PageData;
+	export let form: ActionData;
+
+	export let title = data.dashboard.name;
+	export let description = data.dashboard.description || '';
+	export let colCount = data.dashboard.columns;
+	export let tags = data.dashboard.tags;
+	export let published = data.dashboard.published;
+	export const panelList = data.dashboard.panels;
+	export let panelForm: Panel[] = panelList;
+
+	export let predefinedPanels: any = data.predefinedPanels;
+</script>
+
 <svelte:head>
 	<title>Copy Dashboard</title>
 </svelte:head>
+<main class="container mx-auto space-y-6">
+	<BreadCrumbs />
+	<Helper
+		infoMessage="This is the dashboard editor page. Here you can edit the dashboard you have created. You can change the name, description, number of columns, tags and panels of the dashboard. You can also publish or unpublish the dashboard."
+	/>
+	<DashboardEditor
+		{title}
+		{description}
+		{colCount}
+		{tags}
+		{published}
+		{panelForm}
+		{form}
+		{predefinedPanels}
+	/>
+</main>
